79a75c99778eeea854de8370d91061aa5700b882
Jukka Svahn <web@rahinaa.biz>
2012-03-19 03:01:01 +0200

    Initial commit.

---
+++ cotton.js
@@ -0,0 +1,329 @@
+
+/*!
+ * Rah_textile_bar
+ * @author Jukka Svahn
+ */
+
+(function($, len, createRange, duplicate){
+
+	var opt = {};
+	
+	var methods = {
+		
+		/**
+		 * Initialize
+		 */
+		
+		init : function() {
+			this.click(function(e) {
+				e.preventDefault();
+				opt = {};
+
+				$.each(this.attributes, function(index, attr) {
+					if(attr.name.substr(0, 5) == 'data-') {
+						opt[attr.name.substr(5)] = attr.value;
+					}
+				});
+				
+				opt.field = $('#'+$(this).attr('href').substr(1));
+				opt.field.focus();
+				opt.selection = opt.field.rah_textile_bar('caret');
+				opt.lines = opt.field.val().split(/\r\n|\r|\n/);
+				opt.words = {};
+				
+				opt.selection.words = {
+					start : 0,
+					end : 0,
+					text : [],
+				};
+				
+				opt.selection.lines = {
+					start : 0,
+					end : 0,
+					text : [],
+				};
+				
+				var i = 0, lineend = 0, linestart = 0;
+				
+				$.each(opt.lines, function(linenumber, line){
+					
+					if(linestart > opt.selection.end) {
+						return;
+					}
+				
+					lineend = linestart+line.length;
+						
+					if(lineend >= opt.selection.start) {
+						
+						if(!opt.selection.lines.text[0]) {
+							opt.selection.lines.start = linestart;
+						}
+						
+						opt.selection.lines.text.push(line);
+						opt.selection.lines.end = lineend;
+					}
+					
+					linestart = lineend+1;
+
+					$.each(line.split(' '), function(index, word) {
+						
+						if(opt.selection.start <= i && opt.selection.end >= i) {
+							opt.selection.words.text.push(word);
+						}
+						
+						opt.words[i] = word;
+						i += word.length+1;
+					});
+				});
+				
+				opt.selection.char_before = opt.field.val().substr(opt.selection.start-1, 1);
+				opt.selection.is_empty = (!opt.selection.text);
+				opt.selection.is_whitespace = (!opt.selection.is_empty && !$.trim(opt.selection.text));
+				opt.selection.is_inline = (opt.selection.text.indexOf("\n") == -1);
+				opt.selection.is_linefirst = (
+					opt.selection.start < 1 || 
+					opt.selection.char_before == "\n" || 
+					opt.selection.char_before == "\r"
+				);
+				
+				if(!format[opt.callback]){
+					return;
+				}
+					
+				var f = format[opt.callback]();
+				
+				if(f) {
+					opt.field.val(f);
+				}
+				
+				opt.field.rah_textile_bar('caret', {
+					start : opt.selection.end, 
+					end : opt.selection.end
+				});
+			});
+		},
+		
+		/*!
+		 * Caret code based on jCaret
+		 * @author C. F., Wong (Cloudgen)
+		 * @link http://code.google.com/p/jcaret/
+		 *
+		 * Copyright (c) 2010 C. F., Wong (http://cloudgen.w0ng.hk)
+		 * Licensed under the MIT License:
+		 * http://www.opensource.org/licenses/mit-license.php
+		 */
+		
+		caret : function(options) {
+			
+			var start, end, t = this[0], browser = $.browser.msie;
+			
+			//t.value = t.value.replace(/(\r\n|\r)/gm, "\n");
+			
+			if(
+				typeof options === "object" && 
+				typeof options.start === "number" && 
+				typeof options.end === "number"
+			) {
+				start = options.start;
+				end = options.end;
+			}
+			
+			if(typeof start != "undefined"){
+				
+				if(browser){
+					var selRange = this[0].createTextRange();
+					selRange.collapse(true);
+					selRange.moveStart('character', start);
+					selRange.moveEnd('character', end-start);
+					selRange.select();
+				}
+				
+				else {
+					this[0].selectionStart = start;
+					this[0].selectionEnd = end;
+				}
+				
+				this[0].focus();
+				return this;
+			}
+			
+			else {
+			
+				if(browser){
+					
+					var selection = document.selection;
+					
+					if (this[0].tagName.toLowerCase() != "textarea") {
+						var val = this.val(),
+						range = selection[createRange]()[duplicate]();
+						range.moveEnd("character", val[len]);
+						var s = (range.text == "" ? val[len]:val.lastIndexOf(range.text));
+						range = selection[createRange]()[duplicate]();
+						range.moveStart("character", -val[len]);
+						var e = range.text[len];
+					}
+					
+					else {
+						var range = selection[createRange](),
+						stored_range = range[duplicate]();
+						stored_range.moveToElementText(this[0]);
+						stored_range.setEndPoint('EndToEnd', range);
+						var s = stored_range.text[len] - range.text[len],
+						e = s + range.text[len]
+					}
+				}
+			
+				else {
+					var s = t.selectionStart, 
+					e = t.selectionEnd;
+				}
+			
+				return {
+					start : s,
+					end : e,
+					text : t.value.substring(s,e)
+				};
+			}
+		}
+	};
+	
+	/**
+	 * Replaces selection with Textile markup
+	 * @param string string
+	 * @param int start
+	 * @param int end
+	 */
+	
+	var insert = function(string, start, end) {
+		
+		if(typeof start === "undefined") {
+			start = opt.selection.start;
+		}
+		
+		if(typeof end === "undefined") {
+			end = opt.selection.end;
+		}
+		
+		opt.field.val(opt.field.val().substring(0, start) + string + opt.field.val().substring(end));
+		opt.selection.end = start + string.length;
+	};
+	
+	/**
+	 * Formatting methods
+	 */
+	
+	var format = {
+		
+		/**
+		 * Formats a code block
+		 */
+		
+		code : function() {
+			
+			if(
+				(opt.selection.is_linefirst && opt.selection.is_empty) ||
+				!opt.selection.is_inline
+			) {
+				insert(
+					'bc. ' + $.trim(opt.selection.lines.text.join("\n")),
+					opt.selection.lines.start, 
+					opt.selection.lines.end
+				);
+				return;
+			}
+			
+			insert('@'+opt.selection.text+'@');
+			return;
+		},
+		
+		/**
+		 * Formats lists: ul, ol
+		 */
+		
+		list : function() {
+			
+			var out = [];
+			
+			$.each(opt.selection.lines.text, function(key, line){
+				out.push(( (opt.selection.is_linefirst && opt.selection.is_empty) || $.trim(line) ? opt.bullet + ' ' : '') + line);
+			});
+			
+			out = out.join("\n");
+			
+			insert(
+				out, 
+				opt.selection.lines.start, 
+				opt.selection.lines.end
+			);
+			
+			opt.selection.end = opt.selection.lines.start + out.length;
+		},
+		
+		/**
+		 * Formats simple inline tags: strong, bold, em, ins, del
+		 */
+		
+		inline : function() {
+			var r = !opt.selection.is_whitespace && opt.selection.is_inline ? 
+				opt.before + opt.selection.text + opt.after : 
+				opt.selection.text + opt.before + opt.after;
+			
+			insert(r);
+		},
+		
+		/**
+		 * Formats headings
+		 */
+		
+		heading : function() {
+			insert(
+				opt['level'] +'. ' + $.trim(opt.selection.lines.text.join("\n")) + "\n\n",
+				opt.selection.lines.start, 
+				opt.selection.lines.end
+			);
+		},
+		
+		/**
+		 * Formats normal blocks
+		 */
+		
+		block : function() {
+			insert(
+				opt['tag'] +'. ' + $.trim(opt.selection.lines.text.join("\n")) + "\n\n",
+				opt.selection.lines.start, 
+				opt.selection.lines.end
+			);
+		},
+		
+		/**
+		 * Formats image
+		 */
+		
+		image : function() {
+		},
+		
+		/**
+		 * Adds a link
+		 */
+		
+		link : function() {
+		}
+	
+	};
+
+	$.fn.rah_textile_bar = function(method) {
+		
+		if(methods[method]){
+			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+		}
+		
+		else if(typeof method === 'object' || !method){
+			return methods.init.apply(this, arguments);
+		}
+		
+		else {
+			$.error('[rah_textile_bar: unknown method '+method+']');
+		}
+	};
+
+})(jQuery, 'length', 'createRange', 'duplicate');
